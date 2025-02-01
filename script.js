4fbccbf3619662e4656358b490ef0a1194533e2c
vekariyaparag <vekariyaparag07@gmail.com>
2025-02-01 18:11:25 +0530

    old portfolio work to new portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+alert ("welcome to my website");
+
+document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed('#domains', {
+    strings: [' ', 'Software Engineer.', 'Python Developer.', 'Student..'],
+    typeSpeed: 40,
+    backspeed: 30,
+    loop: true
+  });
+});
